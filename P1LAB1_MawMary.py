a8f03c4e09e83ffaa47b1818f50a223501b9a57e
mawm5346 <mawm5346@student.faytechcc.edu>
2025-06-23 02:15:16 -0400

    Add files via upload

---
+++ P1LAB1_MawMary.py
@@ -0,0 +1,14 @@
+# Your name
+name = input("Enter your name: ")
+
+# Date
+date = input("Enter the date: ")
+
+# Assignment Name
+assignment = input("Assignment Name: ")
+
+# A brief description of the project
+project = input("A brief description of the project: ")
+
+# Welcome message
+print("Welcome to CTI110", name + "!")
